181eaf016e04a07e722531363c541617424282dc
Jerom <jeromjomanthara@gmail.com>
2024-06-15 03:33:18 +0530

    feat(settings): add template folder

--- VideoSearch/settings.py
+++ VideoSearch/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     # custom apps
-    "video.apps.VideoConfig",
+    "videos.apps.VideosConfig",
 ]
 
 MIDDLEWARE = [
@@ -46,15 +46,17 @@ ROOT_URLCONF = "VideoSearch.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR/ 'templates'
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
