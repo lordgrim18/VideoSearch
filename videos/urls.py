2650be520bdb348270f76b1f7aa9b6157b1f1e1b
Jerom <jeromjomanthara@gmail.com>
2024-06-17 04:30:30 +0530

    feat: add search and helper fns

--- videos/urls.py
+++ videos/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('upload/', views.upload_video, name='upload_video'),
-    # path('search/', views.search_subtitles, name='search_subtitles'),
+    path('search/', views.search_subtitles, name='search_subtitles'),
 ]
