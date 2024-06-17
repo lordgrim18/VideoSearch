4dc4136580bfc92fbce8dc913b49a12e68926b36
Jerom <jeromjomanthara@gmail.com>
2024-06-17 22:55:00 +0530

    feat(utils): add fn to save file locally

--- videos/utils.py
+++ videos/utils.py
@@ -1,8 +1,10 @@
 import re
-
+import os
+from django.conf import settings
+from decimal import Decimal
 
 def time_to_seconds(time_str):
-    h, m, s = map(float, time_str.replace(',', '.').split(':'))
+    h, m, s = map(float, time_str.split(',')[0].split(':'))
     return h * 3600 + m * 60 + s
 
 def parse_srt(srt_content):
@@ -12,31 +14,20 @@ def parse_srt(srt_content):
     subtitles = []
     for match in matches:
         subtitles.append({
-            'start': time_to_seconds(match[1]),
-            'end': time_to_seconds(match[2]),
+            'start': Decimal(time_to_seconds(match[1])),
             'text': match[3]
         })
 
     return subtitles
 
-from django.db.models import Q
-from .models import Subtitle
+def save_file_locally(file, video_id):
+    local_dir = os.path.join(settings.MEDIA_ROOT, 'videos')
+    if not os.path.exists(local_dir):
+        os.makedirs(local_dir)
+    
+    file_path = os.path.join(local_dir, f"{video_id}_{file.name}")
+    with open(file_path, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
 
-# def search_subtitle(video_id, keyword=None, start_time=None, end_time=None):
-#     # Start with a query set filtered by video ID
-#     query_set = Subtitle.objects.filter(video__video_id=video_id)
-
-#     # Apply keyword filter if provided
-#     if keyword:
-#         query_set = query_set.filter(text__icontains=keyword)
-
-#     # Apply time filters if provided
-#     if start_time is not None and end_time is not None:
-#         query_set = query_set.filter(Q(start_time__gte=start_time) & Q(end_time__lte=end_time))
-#     elif start_time is not None:
-#         query_set = query_set.filter(start_time__gte=start_time)
-#     elif end_time is not None:
-#         query_set = query_set.filter(end_time__lte=end_time)
-
-#     # Return the filtered query set
-#     return query_set
+    return file_path
